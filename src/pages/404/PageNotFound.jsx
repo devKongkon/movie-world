6b999b580a46c52b6f739a3658fbc3e80da5f76c
devKongkon <developerkongkon@gmail.com>
2023-10-03 18:49:03 +0600

    completed the project

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,8 +1,15 @@
 import './style.scss'
-
+import { Wrapper } from "../../pages/index"
 
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return (
+    <div className="pageNotFound">
+      <Wrapper>
+        <span className='bigText'>404</span>
+        <span className='smallText'>Page not found!</span>
+      </Wrapper>
+    </div>
+  )
 };
 
 export default PageNotFound;
